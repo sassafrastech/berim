7ff050ee07d051b2b560321e8e00f2cc84b16bc2
Jill Dimond <jill@sassafrastech.com>
2014-03-12 17:48:45 -0400

    file uploading and error handling

--- berim.rb
+++ berim.rb
@@ -3,6 +3,8 @@ require 'oauth2'
 require "./creds.rb"
 require "sinatra"
 require 'json'
+require 'base64'
+include FileUtils::Verbose
 
 site_path = 'https://berim.nationbuilder.com'
 client = OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET, :site => site_path)
@@ -11,53 +13,84 @@ token = OAuth2::AccessToken.new(client, TOKEN)
 
 configure{ enable :logging }
 before {
-    env['rack.logger'] = Logger.new("error.log")
+  env['rack.logger'] = Logger.new("error.log")
 }
 
 post '/' do
-  logger.info("received post request")
-  logger.info(params.inspect)
 
-    f = {
-  "attachment" => {
-    "filename"=> "test.jpg",
-    "content_type" => "image/jpeg",
-    "content" => "#{params[:datafile]}"
-    }
-  }
+  begin
+    # if there is a file, upload it and get url
+    file_url = params['datafile'] ? upload_file(params['datafile'], token) : nil
 
-    response = token.post('/api/v1/sites/berim/pages/blogs/7/attachments',
-      :headers => {'Accept' => 'application/json', 'Content-Type' => 'application/json'},
-      :body => JSON.dump(f)
-    )
+    upload_post(params, token, file_url)
 
+    # redirect to success page
+    redirect 'http://www.berim.org/letter_submitted'
+  rescue
+    logger.info("General failure: #{$!}")
+    # render failure
+    send_file('berim-error.html')
+  end
 
+end
 
-  # first upload file
-  file_response = JSON.parse(response)
-  file_url = file_response.nil? ? '' : file_response[:url]
-
-
+# upload post to NationBuilder
+# returns response
+def upload_post(params, token, file_url)
   # build the json for NationBuilder
-  d = {
+
+  img_chunk = file_url.nil? ? '' : "File: <img src='#{file_url}'/>"
+
+  post = {
     "blog_post" => {
       "name" => "New Letter from #{params[:name]}",
       "status" => "drafted",
-      "content_before_flip" => "Name: #{params[:name]}, Email: #{params[:email]}, Letter: #{params[:letter]}"
+      "content_before_flip" => "Name: #{params[:name]}, Email: #{params[:email]}, Letter: #{params[:letter]} #{img_chunk}"
     }
-
   }
-
-  logger.info(JSON.dump(d))
 
   # send it off to nation builder to create the post
   response = token.post('/api/v1/sites/berim/pages/blogs/7/posts',
     :headers => {'Accept' => 'application/json', 'Content-Type' => 'application/json'},
-    :body => JSON.dump(d)
+    :body => JSON.dump(post)
   )
 
-  JSON.parse(response.body)
-  # redirect to nation builder page, thank you.
+  unless response.status == 200
+    logger.info("Blog post failed: #{response.body}")
+    raise 'post failed'
+  end
+
+end
+
+# upload file to NationBuilder page attachments
+# returns url of file on NationBuilder
+def upload_file(file, token)
+
+  # encode attachment
+  encoded_file = Base64.strict_encode64(file[:tempfile].read)
+
+  # create JSON
+  fdata = {
+    "attachment" => {
+      "filename" => params['datafile'][:filename],
+      "content_type" => "image/jpeg",
+      "updated_at" => "2013-06-06T10:15:02-07:00",
+      "content" => encoded_file
+    }
+  }
+
+  # post the request
+  response = token.post('/api/v1/sites/berim/pages/letter_attachments/attachments',
+    :headers => {'Accept' => 'application/json', 'Content-Type' => 'application/json'},
+    :body => JSON.dump(fdata)
+  )
+
+  unless response.status == 200
+    logger.info("Upload failed: #{response.body}")
+    raise 'upload failed'
+  end
+
+  return JSON.parse(response.body)['attachment']['url']
 
 end
 
